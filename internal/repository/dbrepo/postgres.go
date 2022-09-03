cae18da9883aed0dc3e26a6cdcdc91a3f9a32c40
Gleb <margleb.dm@yandex.ru>
2022-09-03 08:43:07 +0300

    Creating the authentication and user database functions

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -2,7 +2,9 @@ package dbrepo
 
 import (
 	"context"
+	"errors"
 	"github.com/margleb/booking/internal/models"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"time"
 )
@@ -145,4 +147,91 @@ func (m *postgresDbRepo) GetRoomByID(id int) (models.Room, error) {
 		return room, err
 	}
 	return room, nil
+}
+
+// GetUserByID - получает пользователя по ID
+func (m *postgresDbRepo) GetUserByID(id int) (models.User, error) {
+	// используем подключение 3 cекунды
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// затем если ничего не происходит, отключаем
+	defer cancel()
+
+	query := `select id, first_name, last_name, email, password, access_level, created_at, updated_at
+    from users where id = $1`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var u models.User
+	err := row.Scan(
+		&u.ID,
+		&u.FirstName,
+		&u.LastName,
+		&u.Email,
+		&u.Password,
+		&u.AccessLevel,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	)
+
+	if err != nil {
+		return u, err
+	}
+
+	return u, nil
+}
+
+// UpdateUser updates a user in the database
+func (m *postgresDbRepo) UpdateUser(u models.User) error {
+	// используем подключение 3 cекунды
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// затем если ничего не происходит, отключаем
+	defer cancel()
+
+	query := `
+    	update users set first_name = $1,
+		last_name = $2,
+		email = $3,
+		access_level = $4,
+		updated_at = $5
+    `
+
+	_, err := m.DB.ExecContext(ctx, query,
+		u.FirstName,
+		u.LastName,
+		u.Email,
+		u.AccessLevel,
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Authenticate - authenticate a user
+func (m *postgresDbRepo) Authenticate(email, testPassword string) (int, string, error) {
+	// используем подключение 3 cекунды
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// затем если ничего не происходит, отключаем
+	defer cancel()
+
+	var id int
+	var hashedPassword string
+
+	row := m.DB.QueryRowContext(ctx, "select id, password from users where email = $1", email)
+	err := row.Scan(&id, &hashedPassword)
+	if err != nil {
+		return id, "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(testPassword))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return 0, "", errors.New("incorrect password")
+	} else if err != nil {
+		return 0, "", err
+	}
+
+	return id, hashedPassword, nil
 }
